faf507e469202f4af3037d119f7d5c5aa3e4b2f9
aman kataria <amankataria1920@gmail.com>
2024-07-15 16:50:47 +0530

    added difference also

--- summing.cpp
+++ summing.cpp
@@ -5,8 +5,11 @@ int main(){
     int a ,b;
     a =1,b = 2;
     cout<<"sum is "<<endl;
-    cout<<a+b;
+    cout<<a+b<<endl;
+
+    cout<<"difference is "<<endl;
+    cout<<a-b;
 
     return 0;
-    
+
 }
